982864ca73a59a38e805f21ac67fe13141ab1720
Cristian <114588928+Nenea21@users.noreply.github.com>
2025-08-23 21:14:50 +0300

    Added "brand" column

--- create_table_scraped_data.sql
+++ create_table_scraped_data.sql
@@ -1,3 +1,7 @@
+-- PROCEDURE: public.create_mouse_data_table()
+
+-- DROP PROCEDURE IF EXISTS public.create_mouse_data_table();
+
 CREATE OR REPLACE PROCEDURE public.create_mouse_data_table(OUT created_table_name TEXT)
 LANGUAGE plpgsql
 AS $BODY$
@@ -15,13 +19,14 @@ BEGIN
         final_table_name := base_table_name || '_' || suffix::TEXT;
         suffix := suffix + 1;
     END LOOP;
-
+    
     create_sql := format('
         CREATE TABLE %I (
             id SERIAL PRIMARY KEY,
             name TEXT,
+			brand TEXT,
             url TEXT,
-            price TEXT,
+            price REAL,  -- Changed to REAL (float) for proper float handling
             tip TEXT,
             interfata_mouse TEXT,
             interfata_receiver TEXT,
@@ -29,9 +34,7 @@ BEGIN
             culoare TEXT,
             event_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );', final_table_name);
-
     EXECUTE create_sql;
-
     created_table_name := final_table_name;
 END;
 $BODY$;
